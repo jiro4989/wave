2d3361a0963c3675f875f9ce8a5448ff4c3ebdcf
jiro4989 <jiroron666@gmail.com>
2020-01-26 13:22:00 +0900

    :bulb: add module description

--- src/wave.nim
+++ src/wave.nim
@@ -1,5 +1,8 @@
 ## wave is a tiny WAV sound module.
 ##
+## It does not support compression/decompression, but it does support mono/stereo.
+## The wave is inspired by `Python wave <https://docs.python.org/3/library/wave.html>`_.
+##
 ## Usage
 ## =====
 ##
